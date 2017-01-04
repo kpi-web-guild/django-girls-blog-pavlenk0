68d2bb38c6e3f3d321e3dbcd095048a9247aed63
Volodymyr Pavlenko <pavlenkov1996@gmail.com>
2017-01-04 16:56:36 +0200

    Add security to the website

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+import django.contrib.auth.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
